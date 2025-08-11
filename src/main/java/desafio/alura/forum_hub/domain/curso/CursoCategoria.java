42333206d2a405ca2a06c326b2f972869cb3299e
Robson Durães <duraesrobson@gmail.com>
2025-08-11 05:54:12 -0300

    fix: categorias criada no enum CursoCategoria

--- src/main/java/desafio/alura/forum_hub/domain/curso/CursoCategoria.java
+++ src/main/java/desafio/alura/forum_hub/domain/curso/CursoCategoria.java
@@ -1,5 +1,17 @@
 package desafio.alura.forum_hub.domain.curso;
 
 public enum CursoCategoria {
-
+    PROGRAMACAO,
+    DESIGN,
+    MARKETING,
+    NEGOCIOS,
+    DATA_SCIENCE,
+    INTELIGENCIA_ARTIFICIAL,
+    SEGURANCA_DA_INFORMACAO,
+    INFRAESTRUTURA,
+    DESENVOLVIMENTO_WEB,
+    MOBILE,
+    CLOUD_COMPUTING,
+    UX_UI,
+    GESTAO_DE_PROJETOS
 }
