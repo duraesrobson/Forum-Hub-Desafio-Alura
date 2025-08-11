22d86bd83933a05c7338e5bbb39b9e238550c023
Robson Durães <duraesrobson@gmail.com>
2025-08-11 02:44:14 -0300

    feat: adicionar properties e Dotenv

--- src/main/java/desafio/alura/forum_hub/ForumHubApplication.java
+++ src/main/java/desafio/alura/forum_hub/ForumHubApplication.java
@@ -3,10 +3,18 @@ package desafio.alura.forum_hub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ForumHubApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ForumHubApplication.class, args);
 	}
 
