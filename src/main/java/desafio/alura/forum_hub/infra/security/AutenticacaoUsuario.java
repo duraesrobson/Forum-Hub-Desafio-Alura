207043311688d9701a6eb125409da2b472ac6dd0
Robson Durães <duraesrobson@gmail.com>
2025-08-12 13:27:22 -0300

    feat: criar classe service de AutenticacaoUsuario implementando UserDetailsService

---
+++ src/main/java/desafio/alura/forum_hub/infra/security/AutenticacaoUsuario.java
@@ -0,0 +1,22 @@
+package desafio.alura.forum_hub.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import desafio.alura.forum_hub.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoUsuario implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username);
+    }
+
+}
--- src/main/java/desafio/alura/forum_hub/repository/UsuarioRepository.java
+++ src/main/java/desafio/alura/forum_hub/repository/UsuarioRepository.java
@@ -1,11 +1,14 @@
 package desafio.alura.forum_hub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import desafio.alura.forum_hub.domain.usuario.Usuario;
 
 @Service
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String username);
 
 }
