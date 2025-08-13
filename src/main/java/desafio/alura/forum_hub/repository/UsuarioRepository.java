357ca3c311ed7007c3325db73a73946f18ebf5f0
Robson Durães <duraesrobson@gmail.com>
2025-08-13 01:00:07 -0300

    fix: correcao para retornar um Usuario no metodo findByEmail

--- src/main/java/desafio/alura/forum_hub/repository/UsuarioRepository.java
+++ src/main/java/desafio/alura/forum_hub/repository/UsuarioRepository.java
@@ -1,7 +1,6 @@
 package desafio.alura.forum_hub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import desafio.alura.forum_hub.domain.usuario.Usuario;
@@ -9,6 +8,6 @@ import desafio.alura.forum_hub.domain.usuario.Usuario;
 @Service
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    UserDetails findByEmail(String username);
+    Usuario findByEmail(String username);
 
 }
